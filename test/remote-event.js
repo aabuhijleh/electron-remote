b675c41fa472ed52a3893bc8b527f4c0595965ea
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2019-06-30 14:10:58 +0300

    fix(Electron 5): enable nodeIntegration

--- test/remote-event.js
+++ test/remote-event.js
@@ -7,7 +7,7 @@ describe('fromRemoteWindow', function() {
   this.timeout(10*1000);
   
   it('should get the ready-to-show event', async function() {
-    let bw = new BrowserWindow({width: 500, height: 500, show: false});
+    let bw = new BrowserWindow({width: 500, height: 500, show: false, webPreferences: {nodeIntegration: true} });
     
     let finished = fromRemoteWindow(bw, 'ready-to-show').take(1).toPromise();
     bw.loadURL('https://www.google.com');
@@ -18,7 +18,7 @@ describe('fromRemoteWindow', function() {
   });
   
   it('should get the dom-ready event', async function() {
-    let bw = new BrowserWindow({width: 500, height: 500, show: false});
+    let bw = new BrowserWindow({width: 500, height: 500, show: false, webPreferences: {nodeIntegration: true} });
     
     let finished = fromRemoteWindow(bw, 'dom-ready', true).take(1).toPromise();
     bw.loadURL('https://www.google.com');
